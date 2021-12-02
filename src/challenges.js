69127be554b99666efa33c391e0ab51c2ec6c003
Cecília Martins <cecilia_lmartins@hotmail.com>
2021-12-02 19:38:14 -0300

    7- pela metade, passou em dois testes e ficou em um

--- src/challenges.js
+++ src/challenges.js
@@ -53,8 +53,14 @@ function highestCount(arrayNumbers) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if (cat1 < cat2) {
+    return "cat1";
+  } else if (cat1 > cat2) {
+    return "cat2"
+  } else if (cat1 === cat2){
+    return "os gatos trombam e o rato foge";
+  }
 }
 
 // Desafio 8
