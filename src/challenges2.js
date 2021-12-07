a0d847960529b77871a1e7dadb741e5f845ade97
Cecília Martins <cecilia_lmartins@hotmail.com>
2021-12-07 18:24:09 -0300

    12 - Pela metade tbm pois preciso de um break

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,13 +4,30 @@ function techList() {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(telefone) {
+/*   
+  let arrayOrdem = telefone.sort(arrayReturn);
+  let arrayReturn = [];
+  for (let posi = 0; posi < telefone.length; posi +=1) {
+    if (telefone.length !== 11) {
+      return "Array com tamanho incorreto.";
+    } else if (telefone[posi] < 0 || telefone[posi] > 9 || telefone.[posi] ) {
+
+    } else if () {
+
+    }
+  }
+    return arrayReturn; */
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  let medidaBaixo = "";
+  //A MEDIDA DE BAIXO TEM Q SER MENOR QUE A DOS OUTROS DOIS LADOS SOMADOS
+  //MEDIDA DE BAIXO VAI SER A C?
+  if ((lineA + lineB) < (lineA + lineC) && (lineB + lineC) < (lado)){
+    medidaBaixo = 
+  }
 }
 
 // Desafio 13
