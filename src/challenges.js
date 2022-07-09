8f6e94d9321aecf6b17bf88f3670654d7a9e937b
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-07-09 15:51:59 -0300

    :wastebasket: Removendo arquivos da trybe

--- src/challenges.js
+++ src/challenges.js
@@ -2,31 +2,30 @@
 function compareTrue(para1, para2) {
   if (para1 === true && para2 === true) {
     return true;
-  } else {
-    return false;
   }
+  return false;
 }
 
 // Desafio 2
 function calcArea(base, height) {
-  return (base * height) /2;
+  return (base * height) / 2;
 }
 
 // Desafio 3
 function splitSentence(umaString) {
   let arrayString = [];
-
-  arrayString = umaString.split(" ");
+  arrayString = umaString.split(' ');
 
   return arrayString;
-} 
+}
 
 // Desafio 4
 function concatName(arrayNames) {
-  let ultimo =  arrayNames[arrayNames.length -1];
+  let ultimo = arrayNames[arrayNames.length - 1];
   let primeiro = arrayNames[0];
-  return ultimo + ", " + primeiro;
-} 
+  const concat = `${ultimo}, ${primeiro}`;
+  return concat;
+}
 
 // Desafio 5
 function footballPoints(wins, ties) {
@@ -37,8 +36,19 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-function highestCount(numbers) {
-//
+function highestCount(arrayN) {
+  let bigger = arrayN[0];
+  let repeat = 1;
+  for (let i = 1; i < arrayN.length; i += 1) {
+    if (bigger === arrayN[i]) {
+      repeat += 1;
+    }
+    if (bigger < arrayN[i]) {
+      bigger = arrayN[i];
+      repeat = 1;
+    }
+  }
+  return repeat;
 }
 
 // Desafio 7
@@ -47,49 +57,51 @@ function catAndMouse(mouse, cat1, cat2) {
   let disCat2 = cat2 - mouse;
 
   if (Math.abs(disCat1) < Math.abs(disCat2)) {
-    return "cat1"
-  } else if (Math.abs(disCat2) < Math.abs(disCat1)) {
-    return "cat2"
-  } else if (Math.abs(disCat1) === Math.abs(disCat2)) {
-    return "os gatos trombam e o rato foge"
+    return 'cat1';
   }
+  if (Math.abs(disCat2) < Math.abs(disCat1)) {
+    return 'cat2';
+  }
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
+const ttt = (param) => {
+  if (param % 3 === 0 && param % 5 === 0) return 'fizzBuzz';
+  if (param % 5 === 0) return 'buzz';
+  if (param % 3 === 0) return 'fizz';
+  return 'bug!';
+};
+
 function fizzBuzz(arrayN) {
   let toReturn = [];
-  for (let i in arrayN) { 
-    if (arrayN[i] % 3 === 0 && arrayN[i] % 5 === 0) {
-        toReturn.push('fizzBuzz');
-    } else if (arrayN[i] % 5 === 0) {
-        toReturn.push('buzz');
-    } else if (arrayN[i] % 3 === 0) {
-        toReturn.push('fizz');
-    } else {
-        toReturn.push('bug!');
-    }
-  } return toReturn;
+  for (let i = 0; i < arrayN.length; i += 1) {
+    toReturn.push(ttt(arrayN[i]));
+  }
+  return toReturn;
 }
 
 // Desafio 9
 function encode(paramString) {
   /* let codificada = paramString.replace(/a/g, /e/g, /i/g, /o/g, /u/g, "1", "2", "3", "4", "5") */
-  let codificada = paramString.replace(/a/g, "1")
-  .replace(/e/g, "2")
-  .replace(/i/g, "3")
-  .replace(/o/g, "4")
-  .replace(/u/g, "5")
-  
+  let codificada = paramString
+    .replace(/a/g, '1')
+    .replace(/e/g, '2')
+    .replace(/i/g, '3')
+    .replace(/o/g, '4')
+    .replace(/u/g, '5');
+
   return codificada;
 }
 function decode(outroParam) {
   /* let decodificada = outroParam.replace(/1/g, /2/g, /3/g, /4/g, /5/g, "a", "e", "i", "o", "u") */
-  let decodificada = outroParam.replace(/1/g, "a")
-  .replace(/2/g, "e")
-  .replace(/3/g, "i")
-  .replace(/4/g, "o")
-  .replace(/5/g, "u")
-  
+  let decodificada = outroParam
+    .replace(/1/g, 'a')
+    .replace(/2/g, 'e')
+    .replace(/3/g, 'i')
+    .replace(/4/g, 'o')
+    .replace(/5/g, 'u');
+
   return decodificada;
 }
 
--- src/challenges2.js
+++ src/challenges2.js
@@ -4,61 +4,49 @@ function techList(tech, name) {
     return 'Vazio!';
   }
   let naOrdem = tech.sort();
-
   const criandoObj = naOrdem.map((cadaTech) => {
     const obj = {
       tech: cadaTech,
-      name: name
+      name,
     };
     return obj;
   });
   return criandoObj;
 }
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Lucas'));
 
 // Desafio 11
-function generatePhoneNumber(telefone) {
-/*   
-  let arrayOrdem = telefone.sort(arrayReturn);
-  let arrayReturn = [];
-  for (let posi = 0; posi < telefone.length; posi +=1) {
-    if (telefone.length !== 11) {
-      return "Array com tamanho incorreto.";
-    } else if (telefone[posi] < 0 || telefone[posi] > 9 || telefone.[posi] ) {
-
-    } else if () {
-
+const funcAuxiliar = (tel) => {
+  if (tel.length < 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let i = 0; i < tel.length; i += 1) {
+    if (tel[i] < 0 || tel[i] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
     }
   }
-    return arrayReturn; */
+};
+
+function generatePhoneNumber(arrayN) {
+  const ttt = funcAuxiliar(arrayN);
+  console.log(ttt);
+  return ttt;
 }
+console.log(funcAuxiliar([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
 
 // Desafio 12
 function triangleCheck(aA, bB, cC) {
-  let qualquer = '';
-  let another = '';
-  if ((aA + bB) < cC) {
-    qualquer = cC;
-    another = aA + bB;
-  } else if ((bB + cC) < aA) {
-    qualquer = aA;
-    another = bB + cC;
-  } else if ((cC + aA) < bB) {
-    qualquer = bB;
-    another = cC + aA;
-  }
-  if (qualquer > another){
-    return false;
-  } else if (qualquer < (Math.abs(qualquer) !== Math.abs(another))) {
-    return false;
-  } else {
+  if (aA < bB + cC && aA > Math.abs(bB - cC)) {
     return true;
   }
+  return false;
+  // meu codigo passou, POREM, o lint deixa ele todo vermelho (E COM TODA RAZÃO KKKKKKKK TAVA ENORME MANO).
+  // não sei bem como fazer referencia, mas esse é o link do codigo que faz o msm q o meu, mas com menos linhas e nenhuma delas vermelha
+  // https://github.com/tryber/sd-019-c-project-playground-functions/pull/142/commits/e13c22c869525b1f76ef73c14d9e5e4cde8410a7
 }
 
 // Desafio 13
-function hydrate(string) {
-  
+function hydrate() {
+
 }
 
 module.exports = {
